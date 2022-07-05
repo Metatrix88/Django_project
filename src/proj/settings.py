6ebb296072f8d77176b7f29f973514318658179a
Andrey Davidovich <andrejdavidovich88@gmail.com>
2022-07-05 22:36:26 +0300

    Two commit

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wug083v&b+ezb)!w+x7xok)eiy+iqukgy0vlxv7s(&-ln@2afa'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['anddavidovich.pythonanywhere.com']
 
 
 # Application definition
