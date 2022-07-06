854ad3e4ab1e284b127f9dcca73640d7bdd36c27
Andrey Davidovich <andrejdavidovich88@gmail.com>
2022-07-06 15:06:49 +0300

    Four commit

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europa/Minsk'
 
 USE_I18N = True
 
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = '/home/anddavidovich/Django_project/static'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = '/home/anddavidovich/Django_project/media'
