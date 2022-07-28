ce87e58247caf47bacd50097db6bfd2e020923aa
Andrey Davidovich <andrejdavidovich88@gmail.com>
2022-07-28 10:31:36 +0300

    main page

--- src/main_page/views.py
+++ src/main_page/views.py
@@ -1,6 +1,7 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from datetime import datetime
+from card_product import models
 
 class HomePage(generic.TemplateView):
     template_name = 'main_page/home.html'
@@ -8,4 +9,5 @@ class HomePage(generic.TemplateView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['today'] = datetime.now().date
+        context['book_list'] = models.Book.objects.all()[:5]
         return context
